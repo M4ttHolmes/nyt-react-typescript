4e9e874dca6d42156b0c5fab14698fe3fb748fc6
Matt Holmes <m4ttholmes@gmail.com>
2021-11-11 18:02:49 -0500

    initial scaffolding set up

---
+++ src/components/ApiFetch.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Display from "./Display";
+
+export default class ApiFetch extends Component {
+
+
+
+
+
+
+
+
+
+    
+    render() {
+        return(
+            <div>
+                <Display />
+            </div>
+        )
+    }
+}
---
+++ src/components/Display.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Display = () => {
+
+    
+
+
+
+
+
+
+    return (
+        <div>
+            <h1>Hello World (From Display.tsx)</h1>
+        </div>
+    )
+}
+
+
+
+export default Display
